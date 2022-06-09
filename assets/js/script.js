fc097f1f3817d315e448d8529178486fb6fef78c
Murilo Kascher <murilokascher@gmail.com>
2022-06-09 11:28:55 -0300

    java pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,16 @@ $('header a').click(function(e){
 	}, 500);
 });
 
+$('footer a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('header').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
 $('.seta-home a').click(function(e){
 	e.preventDefault();
 	var id = $(this).attr('href'),
@@ -19,3 +29,11 @@ $('.seta-home a').click(function(e){
 	}, 500);
 });
 
+//menu fixo com background
+(function () {
+    var menu = document.getElementById('background-fixo'); // colocar em cache
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 500) menu.classList.add('header-fundo-fixo'); // > 0 ou outro valor desejado
+        else menu.classList.remove('header-fundo-fixo');
+    });
+})();
